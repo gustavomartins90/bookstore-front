3ce6da914f4d5385ae732b24ddbdb7243a3c7e32
gustavomartins90 <gustavomartins.trade@gmail.com>
2022-08-14 23:09:25 -0300

    livre create implementado

--- src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
+++ src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Livro } from '../livro.model';
 import { LivroService } from '../livro.service';
 
@@ -16,18 +16,26 @@ export class LivroReadAllComponent implements OnInit {
 
   listLivro: Livro[] = []
 
-  constructor(private service: LivroService, private route: ActivatedRoute) { }
+  constructor(
+    private service: LivroService, 
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.id_cat = this.route.snapshot.paramMap.get('id_cat')!
     this.findAll()
   }
 
-  findAll():void {
+  findAll(): void {
     this.service.findAllByCategoria(this.id_cat).subscribe((resposta) => {
       this.listLivro = resposta;
       console.log(this.listLivro)
     })
+  }
+
+  navegarParaCriarLivro(): void {
+    this.router.navigate([`categorias/${this.id_cat}/livros/create`])
   }
 
 }
